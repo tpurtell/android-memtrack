83b2f8db2937f18a081b83e238d5729d3150b974
T.J. Purtell <tj@mobisocial.us>
2014-09-21 13:13:18 -0700

    initial commit

---
+++ MemTrack/MemTrackForm.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace MemTrack
+{
+    partial class MemTrackForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this._MemoryChart = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this._RecordButton = new System.Windows.Forms.Button();
+            this._SaveButton = new System.Windows.Forms.Button();
+            this._SaveImageButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this._PackageText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this._FrequencySpinner = new System.Windows.Forms.NumericUpDown();
+            this._DirtyCheck = new System.Windows.Forms.CheckBox();
+            this._TrackActivities = new System.Windows.Forms.CheckBox();
+            this._NotFoundLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this._MemoryChart)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._FrequencySpinner)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // _MemoryChart
+            // 
+            chartArea4.AxisX.MajorGrid.LineColor = System.Drawing.Color.LightGray;
+            chartArea4.AxisX.Minimum = 0D;
+            chartArea4.AxisX.Title = "Seconds";
+            chartArea4.AxisY.MajorGrid.LineColor = System.Drawing.Color.LightGray;
+            chartArea4.AxisY.Title = "Memory (MB)";
+            chartArea4.Name = "ChartArea1";
+            this._MemoryChart.ChartAreas.Add(chartArea4);
+            this._MemoryChart.Dock = System.Windows.Forms.DockStyle.Fill;
+            legend4.Name = "Legend1";
+            this._MemoryChart.Legends.Add(legend4);
+            this._MemoryChart.Location = new System.Drawing.Point(3, 3);
+            this._MemoryChart.Name = "_MemoryChart";
+            this._MemoryChart.Size = new System.Drawing.Size(767, 544);
+            this._MemoryChart.TabIndex = 0;
+            this._MemoryChart.Text = "chart1";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.tableLayoutPanel1.Controls.Add(this._MemoryChart, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(905, 550);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel1.Controls.Add(this._RecordButton);
+            this.flowLayoutPanel1.Controls.Add(this._SaveButton);
+            this.flowLayoutPanel1.Controls.Add(this._SaveImageButton);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Controls.Add(this._PackageText);
+            this.flowLayoutPanel1.Controls.Add(this.label2);
+            this.flowLayoutPanel1.Controls.Add(this._FrequencySpinner);
+            this.flowLayoutPanel1.Controls.Add(this._DirtyCheck);
+            this.flowLayoutPanel1.Controls.Add(this._TrackActivities);
+            this.flowLayoutPanel1.Controls.Add(this._NotFoundLabel);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(776, 3);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(126, 544);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // _RecordButton
+            // 
+            this._RecordButton.Location = new System.Drawing.Point(3, 3);
+            this._RecordButton.Name = "_RecordButton";
+            this._RecordButton.Size = new System.Drawing.Size(120, 23);
+            this._RecordButton.TabIndex = 0;
+            this._RecordButton.Text = "Record";
+            this._RecordButton.UseVisualStyleBackColor = true;
+            this._RecordButton.Click += new System.EventHandler(this._RecordButton_Click);
+            // 
+            // _SaveButton
+            // 
+            this._SaveButton.Location = new System.Drawing.Point(3, 32);
+            this._SaveButton.Name = "_SaveButton";
+            this._SaveButton.Size = new System.Drawing.Size(120, 23);
+            this._SaveButton.TabIndex = 1;
+            this._SaveButton.Text = "Save";
+            this._SaveButton.UseVisualStyleBackColor = true;
+            this._SaveButton.Click += new System.EventHandler(this._SaveButton_Click);
+            // 
+            // _SaveImageButton
+            // 
+            this._SaveImageButton.Location = new System.Drawing.Point(3, 61);
+            this._SaveImageButton.Name = "_SaveImageButton";
+            this._SaveImageButton.Size = new System.Drawing.Size(120, 23);
+            this._SaveImageButton.TabIndex = 9;
+            this._SaveImageButton.Text = "Save Image";
+            this._SaveImageButton.UseVisualStyleBackColor = true;
+            this._SaveImageButton.Click += new System.EventHandler(this._SaveImageButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Package";
+            // 
+            // _PackageText
+            // 
+            this._PackageText.Location = new System.Drawing.Point(3, 103);
+            this._PackageText.Name = "_PackageText";
+            this._PackageText.Size = new System.Drawing.Size(120, 20);
+            this._PackageText.TabIndex = 3;
+            this._PackageText.TextChanged += new System.EventHandler(this._PackageText_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Frequency (s)";
+            // 
+            // _FrequencySpinner
+            // 
+            this._FrequencySpinner.Location = new System.Drawing.Point(3, 142);
+            this._FrequencySpinner.Maximum = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this._FrequencySpinner.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this._FrequencySpinner.Name = "_FrequencySpinner";
+            this._FrequencySpinner.Size = new System.Drawing.Size(120, 20);
+            this._FrequencySpinner.TabIndex = 5;
+            this._FrequencySpinner.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // _DirtyCheck
+            // 
+            this._DirtyCheck.AutoSize = true;
+            this._DirtyCheck.Location = new System.Drawing.Point(3, 168);
+            this._DirtyCheck.Name = "_DirtyCheck";
+            this._DirtyCheck.Size = new System.Drawing.Size(71, 17);
+            this._DirtyCheck.TabIndex = 6;
+            this._DirtyCheck.Text = "Dirty Only";
+            this._DirtyCheck.UseVisualStyleBackColor = true;
+            this._DirtyCheck.CheckedChanged += new System.EventHandler(this._DirtyCheck_CheckedChanged);
+            // 
+            // _TrackActivities
+            // 
+            this._TrackActivities.AutoSize = true;
+            this._TrackActivities.Checked = true;
+            this._TrackActivities.CheckState = System.Windows.Forms.CheckState.Checked;
+            this._TrackActivities.Location = new System.Drawing.Point(3, 191);
+            this._TrackActivities.Name = "_TrackActivities";
+            this._TrackActivities.Size = new System.Drawing.Size(99, 17);
+            this._TrackActivities.TabIndex = 8;
+            this._TrackActivities.Text = "Track Activities";
+            this._TrackActivities.UseVisualStyleBackColor = true;
+            // 
+            // _NotFoundLabel
+            // 
+            this._NotFoundLabel.AutoSize = true;
+            this._NotFoundLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._NotFoundLabel.ForeColor = System.Drawing.Color.Crimson;
+            this._NotFoundLabel.Location = new System.Drawing.Point(3, 211);
+            this._NotFoundLabel.Name = "_NotFoundLabel";
+            this._NotFoundLabel.Size = new System.Drawing.Size(111, 13);
+            this._NotFoundLabel.TabIndex = 7;
+            this._NotFoundLabel.Text = "No Process Found";
+            // 
+            // MemTrackForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 550);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MemTrackForm";
+            this.Text = "Android MemTrack";
+            ((System.ComponentModel.ISupportInitialize)(this._MemoryChart)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this._FrequencySpinner)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart _MemoryChart;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button _RecordButton;
+        private System.Windows.Forms.Button _SaveButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox _PackageText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown _FrequencySpinner;
+        private System.Windows.Forms.CheckBox _DirtyCheck;
+        private System.Windows.Forms.Label _NotFoundLabel;
+        private System.Windows.Forms.CheckBox _TrackActivities;
+        private System.Windows.Forms.Button _SaveImageButton;
+    }
+}
+
---
+++ MemTrack/MemTrackForm.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace MemTrack
+{
+    public partial class MemTrackForm : Form
+    {
+        public class MemPoint
+        {
+            public double When { get; set; }
+            public double Pss { get; set; }
+            public double Dirty { get; set; }
+            public string Extras { get; set; }
+        }
+
+        public class Category
+        {
+            public string Name;
+            public Regex Regex;
+            public List<MemPoint> Samples = new List<MemPoint>(); 
+        }
+        public MemTrackForm()
+        {
+            InitializeComponent();
+
+            _Categories.Add(new Category { Name = "TOTAL", Regex = _ReTotal });
+            _Categories.Add(new Category { Name = "Native", Regex = _ReNativeHeap });
+            _Categories.Add(new Category { Name = "Dalvik", Regex = _ReDalvikHeap });
+            _Categories.Add(new Category { Name = "Other", Regex = _ReOtherDev });
+            _Categories.Add(new Category { Name = "SO", Regex = _ReSoMemmap });
+            _Categories.Add(new Category { Name = "Unknown", Regex = _ReUnknown });
+            _Categories.Add(new Category { Name = "APK", Regex = _ReApkMemmap });
+
+            foreach(var c in _Categories)
+            {
+                var s = new Series(c.Name);
+                s.ChartType = SeriesChartType.Line;
+                s.SmartLabelStyle = new SmartLabelStyle {Enabled = true};
+                _MemoryChart.Series.Add(s);
+            }
+        }
+
+        private Task _Recording;
+        private CancellationTokenSource _CancellationSource;
+
+        private bool IsRecording
+        {
+            get { return _Recording != null; }
+        }
+
+        private async Task ToggleRecord()
+        {
+            if (IsRecording)
+            {
+                var existing = _Recording;
+                _CancellationSource.Cancel();
+                await existing;
+                if (existing == _Recording)
+                    _Recording = null;
+                return;
+            }
+            if (_PackageText.Text.Trim().Length == 0)
+            {
+                MessageBox.Show("Must specify package name");
+                return;
+            }
+            Clear();
+            _PackageText.Enabled = false;
+            _TrackActivities.Enabled = false;
+            _RecordButton.Text = "Stop";
+            _Start = DateTime.Now;
+            _Recording = StartRecording();            
+        }
+
+        private static Regex _ReNativeHeap = new Regex(@"Native Heap\s+(\d+)\s+(\d+)");
+        private static Regex _ReDalvikHeap = new Regex(@"Dalvik Heap\s+(\d+)\s+(\d+)");
+        private static Regex _ReDalvikOther = new Regex(@"Dalvik Other\s+(\d+)\s+(\d+)");
+        private static Regex _ReStack = new Regex(@"Stack\s+(\d+)\s+(\d+)");
+        private static Regex _ReOtherDev = new Regex(@"Other dev\s+(\d+)\s+(\d+)");
+        private static Regex _ReSoMemmap = new Regex(@".so mmap\s+(\d+)\s+(\d+)");
+        private static Regex _ReJarMemmap = new Regex(@".jar mmap\s+(\d+)\s+(\d+)");
+        private static Regex _ReApkMemmap = new Regex(@".apk mmap\s+(\d+)\s+(\d+)");
+        private static Regex _ReTtfMemmap = new Regex(@".ttf mmap\s+(\d+)\s+(\d+)");
+        private static Regex _ReDexMemmap = new Regex(@".dex mmap\s+(\d+)\s+(\d+)");
+        private static Regex _ReOtherMemmap = new Regex(@"Other mmap\s+(\d+)\s+(\d+)");
+        private static Regex _ReUnknown = new Regex(@"Unknown\s+(\d+)\s+(\d+)");
+        private static Regex _ReTotal = new Regex(@"TOTAL\s+(\d+)\s+(\d+)");
+        private List<Category> _Categories = new List<Category>();
+        private DateTime _Start;
+        private List<string> _Activities = new List<string>();
+        private bool _Resumed;
+
+        private static Regex _ReResumedActivity = new Regex(@"mResumedActivity:\s*ActivityRecord\s*\{\s*[a-fA-F0-9]+\s*[a-zA-Z0-9]+\s*(\S+)");
+
+        void Clear()
+        {
+            foreach (var c in _Categories)
+                c.Samples.Clear();
+            _Activities.Clear();
+        }
+
+        async Task StartRecordingActivities()
+        {
+            try
+            {
+                var last = default(string);
+                for (;;)
+                {
+                    var p = new Process();
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.FileName = "adb.exe";
+                    p.StartInfo.Arguments = string.Format("shell dumpsys activity package {0}", _PackageText.Text);
+                    p.Start();
+                    var output = await p.StandardOutput.ReadToEndAsync();
+                    var m = _ReResumedActivity.Match(output);
+                    var resumed = default(string);
+                    if (m.Success)
+                    {
+                        resumed = m.Groups[1].Value;
+                        //if (resumed.StartsWith(_PackageText.Text))
+                        //    resumed = resumed.Substring(_PackageText.Text.Length + 1);
+                        //keep it even shorter
+                        if (resumed.LastIndexOf(".") != -1)
+                            resumed = resumed.Substring(resumed.LastIndexOf(".") + 1);
+                    }
+                    if (resumed != null)
+                        _Resumed |= true;
+                    if (resumed != null && resumed != last)
+                        _Activities.Add(resumed);
+                    last = resumed;
+                }
+            }
+            catch (OperationCanceledException e)
+            {
+            }
+
+        }
+
+        async Task StartRecording()
+        {
+            try
+            {
+                _CancellationSource = new CancellationTokenSource();
+                if(_TrackActivities.Checked)
+                    StartRecordingActivities();
+                var last_resumed = false;
+                for (;;)
+                {
+                    var p = new Process();
+                    p.StartInfo.CreateNoWindow = true;
+                    p.StartInfo.UseShellExecute = false;
+                    p.StartInfo.RedirectStandardOutput = true;
+                    p.StartInfo.FileName = "adb.exe";
+                    p.StartInfo.Arguments = string.Format("shell dumpsys meminfo {0}", _PackageText.Text);
+                    p.Start();
+                    var output = await p.StandardOutput.ReadToEndAsync();
+                    var when = (DateTime.Now - _Start).TotalSeconds;
+
+                    var last = _Categories[0].Samples.Count;
+
+                    var extras = default(string);
+                    var items = new List<string>();
+                    if (last_resumed != _Resumed)
+                    {
+                        items.Add(_Resumed ? "FG" : "BG");
+                        last_resumed = _Resumed;
+                    }
+                    _Resumed = false;
+                    foreach (var a in _Activities)
+                        items.Add(a);
+                    _Activities.Clear();
+                    if (items.Count > 0)
+                        extras = string.Join("\n", items);
+
+                    foreach (var c in _Categories)
+                    {
+                        var m = c.Regex.Match(output);
+                        if (!m.Success)
+                            break;
+
+                        var pt = new MemPoint
+                        {
+                            When = when,
+                            Pss = double.Parse(m.Groups[1].Value) / 1000,
+                            Dirty = double.Parse(m.Groups[2].Value) / 1000,
+                            Extras = c.Name == "TOTAL" ? extras : default(string),
+                        };
+                        c.Samples.Add(pt);
+                        Console.WriteLine("{0} {1} {2} {3}", c.Name, pt.When, pt.Pss, pt.Dirty);
+
+                    }
+                    _NotFoundLabel.Visible = _Categories[0].Samples.Count == last;
+                    RefreshData();
+                    await Task.Delay(TimeSpan.FromSeconds((int)_FrequencySpinner.Value), _CancellationSource.Token);
+                }
+            }
+            catch (OperationCanceledException e)
+            {
+                
+            }
+            finally
+            {
+                _PackageText.Enabled = true;
+                _TrackActivities.Enabled = true;
+                _RecordButton.Text = "Record";
+                _Recording = null;
+            }
+        }
+
+        private async Task<bool> CheckAlive()
+        {
+            var p = new Process();
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = "adb.exe";
+            p.StartInfo.Arguments = string.Format("shell dumpsys meminfo {0}", _PackageText.Text);
+            p.Start();
+            var output = await p.StandardOutput.ReadToEndAsync();
+            return !output.Contains("No process found");
+        }
+
+        public void RefreshData()
+        {
+            var i = 0;
+            foreach (var s in _MemoryChart.Series)
+                s.Points.DataBind(_Categories[i++].Samples, "When", _DirtyCheck.Checked ? "Dirty" : "Pss", "Label=Extras");
+        }
+        private async void _PackageText_TextChanged(object sender, EventArgs e)
+        {
+            _NotFoundLabel.Visible = _PackageText.Text.Trim().Length == 0 || !await CheckAlive();
+        }
+
+        private void _DirtyCheck_CheckedChanged(object sender, EventArgs e)
+        {
+            RefreshData();
+        }
+
+        private async void _RecordButton_Click(object sender, EventArgs e)
+        {
+            await ToggleRecord();
+        }
+
+        private void _SaveImageButton_Click(object sender, EventArgs e)
+        {
+            var dlg = new SaveFileDialog();
+            dlg.Filter = "PNG image (*.png)|*.png";
+            dlg.RestoreDirectory = true;
+            var r = dlg.ShowDialog(this);
+            if (r != DialogResult.OK)
+                return;
+            var b = new Bitmap(_MemoryChart.Width, _MemoryChart.Height);
+            _MemoryChart.DrawToBitmap(b, new Rectangle(0, 0, b.Width, b.Height));
+            using (var f = dlg.OpenFile())
+            {
+                b.Save(f, ImageFormat.Png);
+            }
+        }
+
+        private void _SaveButton_Click(object sender, EventArgs e)
+        {
+            var dlg = new SaveFileDialog();
+            dlg.Filter = "Tab delimited table (*.tsv)|*.tsv";
+            dlg.RestoreDirectory = true;
+            var r = dlg.ShowDialog(this);
+            if (r != DialogResult.OK)
+                return;
+            using (var f = new StreamWriter(dlg.OpenFile()))
+            {
+                f.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", "Name", "When", "Pss", "Dirty", "Extras");
+                foreach (var c in _Categories)
+                {
+                    foreach (var s in c.Samples)
+                    {
+                        f.WriteLine("{0}\t{1}\t{2}\t{3}\t{4}", c.Name, s.When, s.Pss, s.Dirty, (s.Extras ??"").Replace("\n", ","));
+                    }
+                }
+            }
+        }
+
+    }
+}
